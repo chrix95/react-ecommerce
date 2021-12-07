b0edd7b52af33a6a417352bff5a169ae5d747771
Christopher Okokon Ntuk <engchris95@gmail.com>
2021-12-08 00:01:56 +0100

    Updated the category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,21 +4,38 @@ import CategoryItem from "./CategoryItem"
 import { mobile } from '../responsive'
 
 const Container = styled.div`
-    display: flex;
     padding: 20px;
+`
+    
+const CategoryList = styled.div`
+    display: flex;
     justify-content: space-between;
+    font-weight: 600;
     ${mobile({ padding: '0px', flexDirection: 'column' })}
+`
+
+const Title = styled.h2`
+    font-size: 40px;
+    font-weight: 100;
+    text-align: center;
+    margin-bottom: 30px;
+    ${mobile({ fontSize: '24px', marginBottom: '10px', fontWeight: 300 })}
 `
 
 const Categories = () => {
     return (
-        <Container>
-            {
-                categories.map(category => (
-                    <CategoryItem category={category} key={category.id} />
-                ))
-            }
-        </Container>
+        <>
+            <Container>
+                <Title>CATEGORY</Title>
+                <CategoryList>
+                    {
+                        categories.map(category => (
+                            <CategoryItem category={category} key={category.id} />
+                        ))
+                    }
+                </CategoryList>
+            </Container>
+        </>
     )
 }
 
